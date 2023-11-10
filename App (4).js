0b3bb373217ebd9a27632f1c17226f33ad9d0077
kommi-prathyusha <143810069+kommi-prathyusha@users.noreply.github.com>
2023-11-10 19:48:42 +0530

    Add files via upload

---
+++ App (4).js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AgeCalculator from './components/AgeCalculator';
+
+function App() {
+  return (
+    <div>
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
